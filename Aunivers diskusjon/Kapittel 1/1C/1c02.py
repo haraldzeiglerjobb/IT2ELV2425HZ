07746517ba312fc40023d67cddf59bf8e4f99af8
Harald Zeigler Jobb <49240797+haraldzeiglerjobb@users.noreply.github.com>
2024-09-15 23:23:52 +0200

    j

---
+++ Aunivers diskusjon/Kapittel 1/1C/1c02.py
@@ -0,0 +1,24 @@
+#Partall
+
+for i in range(-100, 101, 2):
+    print(i, ",  ", end = "")
+print()
+print("..........")
+
+i = -100
+while i <=100:
+    print(i, ",  ",end = "")
+    i +=2
+print()
+print("..........")
+
+#Oddetall
+for i in range(73,-73,-2):
+    print(i, ",  ", end = "")
+print()
+print("..........")
+
+i = 73
+while i >=-73:
+    print(i, ",  ", end = "")
+    i -=2
