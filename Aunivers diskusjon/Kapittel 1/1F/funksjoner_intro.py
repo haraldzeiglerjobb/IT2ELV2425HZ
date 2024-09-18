cb1a1573408679ad149a46cfc04b0eaac5a10d91
Harald Zeigler Jobb <49240797+haraldzeiglerjobb@users.noreply.github.com>
2024-09-18 15:39:36 +0200

    c

--- Aunivers diskusjon/Kapittel 1/1F/funksjoner_intro.py
+++ Aunivers diskusjon/Kapittel 1/1F/funksjoner_intro.py
@@ -16,12 +16,13 @@ h(1,2,3,4,5,6) #argumentene er de du faktisk sender inn
 #x = 1, y = 2, z = 3, æ = 4, ø = 5, å = 6
 h(x =9, y = 8, z = 5, ø = 1, å = 0, æ = 5)
 
-def k(x = 0,y = 5,z= 9):
+def k(x: int = 0,y: int = 5,z: float = 9.0):
     print("hei:::k")
     print(x,y,z)
+    print(x*y*z)
 
 k()
 
 #teste: hva skjer hvis vi skriver noen ,men ikke alle, og 
 k(x=9)
-k(1)
+k(1.5,2.6,"hei")
