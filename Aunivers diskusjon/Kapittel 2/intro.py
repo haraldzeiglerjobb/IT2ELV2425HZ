714ddacfe0846dcb1b7d5612f36e6e7fd7dd0af2
Harald Zeigler Jobb <49240797+haraldzeiglerjobb@users.noreply.github.com>
2024-10-25 15:06:34 +0200

    Update intro.py

--- Aunivers diskusjon/Kapittel 2/intro.py
+++ Aunivers diskusjon/Kapittel 2/intro.py
@@ -21,10 +21,20 @@ class Appelsin(Frukt):
     #klassevariabler
     #vekt = "høy"
     
-    def __init__(sjøl, produsent, land = "Spania", vekt: int =100):
+    def __init__(sjøl, produsent, farge: str = "oransje", land = "Spania", vekt: int =100):
         #Konstruktør
-        super().__init__(produsent)
+        super().__init__(produsent, farge)
+        sjøl.vekt = vekt
+        sjøl.land = land
+
+    def objektmetode(sjøl):
+        #mal for objektmetoder... husk "sjøl"
+        pass
+
+    def print(sjøl):
+        print(sjøl.farge, sjøl.vekt, sjøl.produsent, sjøl.land)
 
 harald = Appelsin("Bama")
 print(harald.produsent)
 print(Appelsin.farge)
+harald.print()
