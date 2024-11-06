610e1e1ad548849811079aad6bb70fff63b5f290
Harald Zeigler Jobb <49240797+haraldzeiglerjobb@users.noreply.github.com>
2024-11-06 11:05:02 +0100

    basic pygame

--- Aunivers diskusjon/Kapittel 2/begreper_abstraksjon.py
+++ Aunivers diskusjon/Kapittel 2/begreper_abstraksjon.py
@@ -3,3 +3,11 @@ Abstraksjon er ...
 Det motsatte av konkretisering
 Ta en ting 
 """
+
+class Hund:
+
+    def __init__(self, rase: str = "Cocker Spaniel", øyenfarge: str = "rød", hale: str = "kort", personnummer: int = 0):
+        self.rase = rase
+        self.øynfarge = øyenfarge
+        self.hale = hale
+        self.personnummer = personnummer
